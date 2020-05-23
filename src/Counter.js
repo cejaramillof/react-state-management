7205b1f18108ffd65d62dfcfbae18aa22c1916ed
Carlos Jaramillo <Carlos.Jaramillo@blackboard.com>
2020-05-23 16:08:21 -0500

    setstate, class, and asynchronicity

--- src/Counter.js
+++ src/Counter.js
@@ -1,14 +1,47 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 3,
+    };
+    this.increment = this.increment.bind(this);
+  }
+
+  increment() {
+    console.log(this.state.count); // print 3, because setState() is Asynchronous
+    this.setState({ count: this.state.count + 1 });
+    console.log(this.state.count); // print 3, because setState() is Asynchronous
+    this.setState({ count: this.state.count + 3 }); // Like in object merge, there's duplicate keys, the last one wins
+    console.log(this.state.count); // print 3, because setState() is Asynchronous // Queuing upstate changes
+
+    /*
+     when this function finish, count will be 6, because react:
+      1. will batch them up,
+      2. figure out the result
+      3. efficiently make that change.
+    */
+  }
+
+  decrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  }
+
+  reset() {
+    this.setState({ count: 0 });
+  }
+
   render() {
+    const { count } = this.state;
+
     return (
       <div className="Counter">
-        <p className="count">0</p>
+        <p className="count">{count}</p>
         <section className="controls">
-          <button>Increment</button>
-          <button>Decrement</button>
-          <button>Reset</button>
+          <button onClick={this.increment}>Increment</button>
+          <button onClick={this.decrement}>Decrement</button>
+          <button onClick={this.reset}>Reset</button>
         </section>
       </div>
     );
