298c8ca2246ccd3c0dd8e700d05058aaabc84cd7
Carlos Jaramillo <Carlos.Jaramillo@blackboard.com>
2020-06-06 09:36:33 -0500

    abstract the lofic of undoReducer

--- src/useReducer/GrudgeContext.js
+++ src/useReducer/GrudgeContext.js
@@ -28,6 +28,39 @@ const reducer = (state, action) => {
   return state;
 };
 
+const useUndoReducer = (reducer, initialState) => {
+  const undoState = {
+    past: [],
+    present: initialState,
+    future: [],
+  };
+
+  const undoReducer = (state, action) => {
+    const newPresent = reducer(state.present, action);
+    if (action.type === UNDO) {
+      const [newPresent, ...newPast] = state.past;
+      return {
+        past: newPast,
+        present: newPresent,
+        future: [state.present, ...state.future],
+      };
+    }
+    if (action.type === REDO) {
+      const [newPresent, ...newFuture] = state.future;
+      return {
+        past: [state.present, ...state.past],
+        present: newPresent,
+        future: newFuture,
+      };
+    }
+    return {
+      past: [state.present, ...state.past],
+      present: newPresent,
+      future: [],
+    }
+  }
+  return useReducer(undoReducer, undoState);
+}
 const reducerUndoRedo = (state = defaultState, action) => {
   if (action.type === GRUDGE_ADD) {
     const newPresent = [
@@ -83,7 +116,8 @@ const defaultState = {
 
 export const GrudgeProvider = ({ children }) => {
   // const [grudges, dispatch] = useReducer(reducer, initialState);
-  const [state, dispatch] = useReducer(reducerUndoRedo, defaultState);
+  // const [state, dispatch] = useReducer(reducerUndoRedo, defaultState);
+  const [state, dispatch] = useUndoReducer(reducer, initialState);
   const grudges = state.present;
   const isPast = !!state.past.length;
   const isFuture = !!state.future.length;
